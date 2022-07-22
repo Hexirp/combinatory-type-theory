7b0ebcdd4558a8f30863ed1e8e0ecb6e43071309
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2022-07-22 18:40:08 +0900

    Update: 単純化する

--- Main.v
+++ Main.v
@@ -44,302 +44,145 @@ Notation
 (** [FI] を定義します。 *)
 
 Definition
-  FI@{i | }
-    : forall A : Type@{i}, A -> A
-    := fun A : Type@{i} => fun x : A => x
+  FI@{i | } (A : Type@{i})
+    : A -> A
+    := fun x : A => x
 .
 
 (** [FK] を定義します。 *)
 
 Definition
-  FK@{i | }
-    : forall A : Type@{i}, forall B : Type@{i}, B -> A -> B
-    := fun A : Type@{i} => fun B : Type@{i} => fun y : B => fun x : A => y
+  FK@{i | } (A : Type@{i}) (B : Type@{i})
+    : B -> A -> B
+    := fun (y : B) (x : A) => y
+.
+
+(** [FB] を定義します。 *)
+
+Definition
+  FB@{i | } (A : Type@{i}) (B : Type@{i}) (C : B -> Type@{i})
+    : (forall y : B, C y) -> forall (y : A -> B) (x : A), C (y x)
+    := fun (z : forall y : B, C y) (y : A -> B) (x : A) => z (y x)
 .
 
 (** [FB0] を定義します。 *)
 
 Definition
-  FB0@{i | }
-    :
-      forall A : Type@{i},
-        forall B : Type@{i},
-          forall C : Type@{i},
-            (B -> C) -> (A -> B) -> A -> C
-    :=
-      fun A : Type@{i} =>
-        fun B : Type@{i} =>
-          fun C : Type@{i} =>
-            fun z : B -> C =>
-              fun y : A -> B =>
-                fun x : A =>
-                  z (y x)
+  FB0@{i si | i < si} (A : Type@{i}) (B : Type@{i}) (C : Type@{i})
+    : (B -> C) -> (A -> B) -> A -> C
+    := FB@{i} A B (FK@{si} B Type@{i} C)
 .
 
 (** [FB1] を定義します。 *)
 
 Definition
-  FB1@{i | }
-    :
-      forall A : Type@{i},
-        forall B : Type@{i},
-          forall C : B -> Type@{i},
-            (forall y : B, C y)
-              ->
-                forall y : A -> B,
-                  forall x : A,
-                    C (y x)
-    :=
-      fun A : Type@{i} =>
-        fun B : Type@{i} =>
-          fun C : B -> Type@{i} =>
-            fun z : forall y : B, C y =>
-              fun y : A -> B =>
-                fun x : A =>
-                  z (y x)
+  FB1@{i | } (A : Type@{i}) (B : Type@{i}) (C : B -> Type@{i})
+    : (forall y : B, C y) -> forall (y : A -> B) (x : A), C (y x)
+    := FB@{i} A B C
+.
+
+(** [FC] を定義します。 *)
+
+Definition
+  FC@{i | } (A : Type@{i}) (B : Type@{i}) (C : A -> B -> Type@{i})
+    : (forall (x : A) (y : B), C x y) -> forall (y : B) (x : A), C x y
+    := fun (z : forall (x : A) (y : B), C x y) (y : B) (x : A) => z x y
 .
 
 (** [FC0] を定義します。 *)
 
 Definition
-  FC0@{i | }
-    :
-      forall A : Type@{i},
-        forall B : Type@{i},
-          forall C : Type@{i},
-            (A -> B -> C) -> B -> A -> C
-    :=
-      fun A : Type@{i} =>
-        fun B : Type@{i} =>
-          fun C : Type@{i} =>
-            fun z : A -> B -> C =>
-              fun y : B =>
-                fun x : A =>
-                  z x y
+  FC0@{i si | i < si} (A : Type@{i}) (B : Type@{i}) (C : Type@{i})
+    : (A -> B -> C) -> B -> A -> C
+    := FC@{i} A B (FK@{si} A (B -> Type@{i}) (FK@{si} B Type@{i} C))
 .
 
 (** [FC1] を定義します。 *)
 
 Definition
-  FC1@{i | }
-    :
-      forall A : Type@{i},
-        forall B : Type@{i},
-          forall C : B -> Type@{i},
-            (A -> forall y : B, C y)
-              ->
-                forall y : B,
-                  A -> C y
-    :=
-      fun A : Type@{i} =>
-        fun B : Type@{i} =>
-          fun C : B -> Type@{i} =>
-            fun z : A -> forall y : B, C y =>
-              fun y : B =>
-                fun x : A =>
-                  z x y
+  FC1@{i si | i < si} (A : Type@{i}) (B : Type@{i}) (C : B -> Type@{i})
+    : (A -> forall y : B, C y) -> forall y : B, A -> C y
+    := FC@{i} A B (FK@{si} A (B -> Type@{i}) C)
 .
 
 (** [FC2] を定義します。 *)
 
 Definition
-  FC2@{i | }
-    :
-      forall A : Type@{i},
-        forall B : Type@{i},
-          forall C : A -> Type@{i},
-            (forall x : A, B -> C x)
-              ->
-                B
-                  ->
-                    forall x : A,
-                      C x
-    :=
-      fun A : Type@{i} =>
-        fun B : Type@{i} =>
-          fun C : A -> Type@{i} =>
-            fun z : forall x : A, B -> C x =>
-              fun y : B =>
-                fun x : A =>
-                  z x y
+  FC2@{i si ssi | i < si, si < ssi} (A : Type@{i}) (B : Type@{i}) (C : A -> Type@{i})
+    : (forall x : A, B -> C x) -> B -> forall x : A, C x
+    := FC@{i} A B (FB0@{si ssi} A Type@{i} (B -> Type@{i}) (FK@{si} B Type@{i}) C)
 .
 
 (** [FC3] を定義します。 *)
 
 Definition
-  FC3@{i | }
-    :
-      forall A : Type@{i},
-        forall B : Type@{i},
-          forall C : A -> B -> Type@{i},
-            (forall x : A, forall y : B, C x y)
-              ->
-                forall y : B,
-                  forall x : A,
-                    C x y
-    :=
-      fun A : Type@{i} =>
-        fun B : Type@{i} =>
-          fun C : A -> B -> Type@{i} =>
-            fun z : forall x : A, forall y : B, C x y =>
-              fun y : B =>
-                fun x : A =>
-                  z x y
+  FC3@{i | } (A : Type@{i}) (B : Type@{i}) (C : A -> B -> Type@{i})
+    : (forall (x : A) (y : B), C x y) -> forall (y : B) (x : A), C x y
+    := FC@{i} A B C
+.
+
+(** [FS] を定義します。 *)
+
+Definition
+  FS@{i | } (A : Type@{i}) (B : A -> Type@{i}) (C : forall x : A, B x -> Type@{i})
+    : (forall (x : A) (y : B x), C x y) -> forall (y : forall x : A, B x) (x : A), C x (y x)
+    := fun (z : forall (x : A) (y : B x), C x y) (y : forall x : A, B x) (x : A) => z x (y x)
 .
 
 (** [FS0] を定義します。 *)
 
 Definition
-  FS0@{i | }
-    :
-      forall A : Type@{i},
-        forall B : Type@{i},
-          forall C : Type@{i},
-            (A -> B -> C) -> (A -> B) -> A -> C
-    :=
-      fun A : Type@{i} =>
-        fun B : Type@{i} =>
-          fun C : Type@{i} =>
-            fun z : A -> B -> C =>
-              fun y : A -> B =>
-                fun x : A =>
-                  z x (y x)
+  FS0@{i si | i < si} (A : Type@{i}) (B : Type@{i}) (C : Type@{i})
+    : (A -> B -> C) -> (A -> B) -> A -> C
+    := FS@{i} A (FK@{si} A Type@{i} B) (FK@{si} A (B -> Type@{i}) (FK@{si} B Type@{i} C))
 .
 
 (** [FS1] を定義します。 *)
 
 Definition
-  FS1@{i | }
-    :
-      forall A : Type@{i},
-        forall B : Type@{i},
-          forall C : B -> Type@{i},
-            (A -> forall y : B, C y)
-              ->
-                forall y : A -> B,
-                  forall x : A,
-                    C (y x)
-    :=
-      fun A : Type@{i} =>
-        fun B : Type@{i} =>
-          fun C : B -> Type@{i} =>
-            fun z : A -> forall y : B, C y =>
-              fun y : A -> B =>
-                fun x : A =>
-                  z x (y x)
+  FS1@{i si | i < si} (A : Type@{i}) (B : Type@{i}) (C : B -> Type@{i})
+    : (A -> forall y : B, C y) -> forall (y : A -> B) (x : A), C (y x)
+    := FS@{i} A (FK@{si} A Type@{i} B) (FK@{si} A (B -> Type@{i}) C)
 .
 
 (** [FS2] を定義します。 *)
 
 Definition
-  FS2@{i | }
-    :
-      forall A : Type@{i},
-        forall B : Type@{i},
-          forall C : A -> Type@{i},
-            (forall x : A, B -> C x)
-              ->
-                (A -> B)
-                  ->
-                    forall x : A,
-                      C x
-    :=
-      fun A : Type@{i} =>
-        fun B : Type@{i} =>
-          fun C : A -> Type@{i} =>
-            fun z : forall x : A, B -> C x =>
-              fun y : A -> B =>
-                fun x : A =>
-                  z x (y x)
+  FS2@{i si ssi | i < si, si < ssi} (A : Type@{i}) (B : Type@{i}) (C : A -> Type@{i})
+    : (forall x : A, B -> C x) -> (A -> B) -> forall x : A, C x
+    := FS@{i} A (FK@{si} A Type@{i} B) (FB0@{si ssi} A Type@{i} (B -> Type@{i}) (FK@{si} B Type@{i}) C)
 .
 
 (** [FS3] を定義します。 *)
 
 Definition
-  FS3@{i | }
-    :
-      forall A : Type@{i},
-        forall B : Type@{i},
-          forall C : A -> B -> Type@{i},
-            (forall x : A, forall y : B, C x y)
-              ->
-                forall y : A -> B,
-                  forall x : A,
-                    C x (y x)
-    :=
-      fun A : Type@{i} =>
-        fun B : Type@{i} =>
-          fun C : A -> B -> Type@{i} =>
-            fun z : forall x : A, forall y : B, C x y =>
-              fun y : A -> B =>
-                fun x : A =>
-                  z x (y x)
+  FS3@{i si | i < si} (A : Type@{i}) (B : Type@{i}) (C : A -> B -> Type@{i})
+    : (forall (x : A) (y : B), C x y) -> forall (y : A -> B) (x : A), C x (y x)
+    := FS@{i} A (FK@{si} A Type@{i} B) C
 .
 
 (** [FS4] を定義します。 *)
 
 Definition
-  FS4@{i | }
-    :
-      forall A : Type@{i},
-        forall B : A -> Type@{i},
-          forall C : Type@{i},
-            (forall x : A, B x -> C) -> (forall x : A, B x) -> A -> C
-    :=
-      fun A : Type@{i} =>
-        fun B : A -> Type@{i} =>
-          fun C : Type@{i} =>
-            fun z : forall x : A, B x -> C =>
-              fun y : forall x : A, B x =>
-                fun x : A =>
-                  z x (y x)
+  FS4@{i si | i < si} (A : Type@{i}) (B : A -> Type@{i}) (C : Type@{i})
+    : (forall x : A, B x -> C) -> (forall x : A, B x) -> A -> C
+    := FS@{i} A B (fun x : A => FK@{si} (B x) Type@{i} C)
 .
 
 (** [FS6] を定義します。 *)
 
 Definition
-  FS6@{i | }
-    :
-      forall A : Type@{i},
-        forall B : A -> Type@{i},
-          forall C : A -> Type@{i},
-            (forall x : A, B x -> C x)
-              ->
-                (forall x : A, B x)
-                  ->
-                    forall x : A,
-                      C x
-    :=
-      fun A : Type@{i} =>
-        fun B : A -> Type@{i} =>
-          fun C : A -> Type@{i} =>
-            fun z : forall x : A, B x -> C x =>
-              fun y : forall x : A, B x =>
-                fun x : A =>
-                  z x (y x)
+  FS6@{i si | i < si} (A : Type@{i}) (B : A -> Type@{i}) (C : A -> Type@{i})
+    : (forall x : A, B x -> C x) -> (forall x : A, B x) -> forall x : A, C x
+    := FS@{i} A B (fun x : A => FK@{si} (B x) Type@{i} (C x))
 .
 
 (** [FS7] を定義します。 *)
 
 Definition
-  FS7@{i | }
-    :
-      forall A : Type@{i},
-        forall B : A -> Type@{i},
-          forall C : forall x : A, B x -> Type@{i},
-            (forall x : A, forall y : B x, C x y)
-              ->
-                forall y : forall x : A, B x,
-                  forall x : A,
-                    C x (y x)
-    :=
-      fun A : Type@{i} =>
-        fun B : A -> Type@{i} =>
-          fun C : forall x : A, B x -> Type@{i} =>
-            fun z : forall x : A, forall y : B x, C x y =>
-              fun y : forall x : A, B x =>
-                fun x : A =>
-                  z x (y x)
+  FS7@{i | } (A : Type@{i}) (B : A -> Type@{i}) (C : forall x : A, B x -> Type@{i})
+    : (forall (x : A) (y : B x), C x y) -> forall (y : forall x : A, B x) (x : A), C x (y x)
+    := FS@{i} A B C
 .
 
 (** [TU] を定義します。 *)
@@ -355,7 +198,7 @@ Definition
 Definition
   TP0@{i | }
     : Type@{i} -> Type@{i} -> Type@{i}
-    := fun A : Type@{i} => fun B : Type@{i} => A -> B
+    := fun (A : Type@{i}) (B : Type@{i}) => A -> B
 .
 
 (** [TP1] を定義します。 *)
@@ -363,7 +206,7 @@ Definition
 Definition
   TP1@{i | }
     : forall A : Type@{i}, (A -> Type@{i}) -> Type@{i}
-    := fun A : Type@{i} => fun B : A -> Type@{i} => forall x : A, B x
+    := fun (A : Type@{i}) (B : A -> Type@{i}) => forall x : A, B x
 .
 
 Section Assertion.
